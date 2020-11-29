1674376d5b4eb86746f1f9cd85cf8722c2a2ed18
Christopher Hogan <christopherhoganjr@icloud.com>
2020-11-29 11:21:50 -0800

    Create bandNameGenerator.py
    Creates a band name with user's input.

---
+++ bandNameGenerator.py
@@ -0,0 +1,8 @@
+# Greeting
+print('Welcome to the Band Name Generator.')
+# Store the user's hometown
+cityName = input('What\'s the name of the city you grew up in?\n')
+# Store the user's pet name
+petName = input('What\'s the name of the last pet you had?\n')
+# Print the combination of strings and variables
+print('Your band name is: ' + cityName + ' ' + petName)
