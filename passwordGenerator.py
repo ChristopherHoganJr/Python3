7c9c381173908815b2f1358edbe73bad7ad86e5d
Christopher Hogan <christopherhoganjr@icloud.com>
2020-12-03 13:22:56 -0800

    updated variable names for readability

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -5,18 +5,18 @@ number = ['0','2','3','4','5','6','7','8','9']
 symbols = ['!','#','$','%','&','(',')','*','+']
 
 print('Welcome to the PyPassword Generator!')
-nr_letters = int(input('How many letters would you like in your password?\n'))
-nr_symbols = int(input('How many symbols would you like?\n'))
-nr_numbers = int(input('How many numbers would you like?\n'))
+user_letters = int(input('How many letters would you like in your password?\n'))
+user_symbols = int(input('How many symbols would you like?\n'))
+user_numbers = int(input('How many numbers would you like?\n'))
 
 # The list for the random characters
 user_pw = []
 # Iterate over the variable lists and add random variables to user_pw
-for i in range(nr_letters):
+for i in range(user_letters):
     user_pw.append(letters[random.randint(0, len(letters)-1)])
-for i in range(nr_numbers):
+for i in range(user_numbers):
     user_pw.append(number[random.randint(0, len(number)-1)])
-for i in range(nr_symbols):
+for i in range(user_symbols):
     user_pw.append(symbols[random.randint(0, len(symbols)-1)])
 # Shuffle the list items
 random.shuffle(user_pw)
