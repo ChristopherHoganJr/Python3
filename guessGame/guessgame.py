05e40d2cdf7d1e05941461d104cbac1bcb9ff050
Christopher Hogan <christopherhoganjr@icloud.com>
2020-12-11 17:59:43 -0800

    updated UI

--- guessGame/guessgame.py
+++ guessGame/guessgame.py
@@ -21,19 +21,26 @@ os.system('cls')
 print(f'You\'ve selected {gameMode.title()}. Let\'s begin.\n')
 # Set point to exit game loop if player guesses correctly
 win = False
+
+# Initiate hints for player
+lowMin = 0
+highMax = 100
 # Game loop
 while guesses > 0 and win == False:
-    # Testing Purposes
-    #print(game_number)
-    #print(guesses)
     # Game Logic
+    print(f'The number is between {lowMin} and {highMax}\n')
+    print(f'Available Guesses: {guesses}')
     user_guess = int(input('What is your guess?: '))
     if user_guess > game_number:
         guesses -= 1
-        print(f'{user_guess} is too high.')
+        os.system('cls')
+        print(f'{user_guess} is too high.\n')
+        highMax = user_guess
     elif user_guess < game_number:
         guesses -= 1
-        print(f'{user_guess} is too low.')
+        os.system('cls')
+        print(f'{user_guess} is too low.\n')
+        lowMin = user_guess
     else:
         win = True
 
@@ -43,4 +50,5 @@ if guesses == 0:
 # Player has guessed correctly and won the game
 if win == True:
     os.system('cls')
-    print(f'You did it! You guessed the {game_number} correctly! You win!')
+    print(f'You did it! You guessed {game_number} correctly! You win!')
+    print(f'Guesses left: {guesses}')
